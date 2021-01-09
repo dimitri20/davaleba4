d72339104b21b4465fb26d436e2a5679ec5e9d0b
dimitri20 <dimitri.gulua@geolab.edu.ge>
2021-01-10 01:25:40 +0400

    Add project files.

---
+++ ArrayFill/ArrayFill/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ArrayFill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[15];
+            int count = 0;
+            int countEven = 0;
+
+            Console.WriteLine("sheiyvanet masivshi chasaweri ricxvi.");
+            Console.WriteLine("shesachereblad sheiyvanet aso 'a'.");
+            Console.WriteLine("ricxvebis maqsimaluri raodenobaa 15.\n\n\n");
+
+            //masivis shevseba
+            while (true)
+            {
+                count++;
+                Console.Write("sheiyvanet mteli ricxvi:  ");
+                string input = Console.ReadLine();
+                if(input == "a" || count == 15)
+                {
+                    break;
+                }
+                arr[count] = Convert.ToInt32(input);
+            }
+
+            Console.Write("{");
+            //luwebis datvla
+            for(int i = 0; i < count; i++)
+            {
+                Console.Write($"{i}, ");
+                if(arr[i] %2 == 0)
+                {
+                    countEven++;
+                }
+            }
+            Console.Write("}\n");
+
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine($"masivshi luwi ricxvebis raodenobaa: {countEven}");
+        }
+    }
+}
---
+++ calculatorWithSwitch/calculatorWithSwitch/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace calculatorWithSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kalkulatori: \n\n");
+
+            while (true)
+            {
+                Console.Write("sheiyvanet pirveli ricxvi:   ");
+                double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("sheiyvanet operacia: ");
+                string operation = Console.ReadLine();
+
+                Console.Write("sheiyvanet meore ricxvi:  ");
+                double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+                double final = 0;
+                bool canDisplay = true;
+
+                switch (operation)
+                {
+                    case "+":
+                        final = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        final = firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        final = firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        if(secondNumber != 0)
+                        {
+                            final = firstNumber / secondNumber;
+                        } else
+                        {
+                            canDisplay = false;
+                            Console.WriteLine("nulze gayofa ar sheidzleba ");
+                            Console.WriteLine("scadet tavidan \n");
+                        }
+                        
+                        break;
+                    default:
+                        Console.WriteLine("tqven sheiyvanet araswori operacia");
+                        Console.WriteLine("scadet tavidan \n");
+                        break;
+                }
+
+                if (canDisplay)
+                {
+                    Console.WriteLine($"{firstNumber} {operation} {secondNumber} = {final}");
+                }
+                
+            }
+        }
+    }
+}
